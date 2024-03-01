8bffc9b97203fe6df1d67d76291db2919abd6b6d
patrickq2023 <151511696+patrickq2023@users.noreply.github.com>
2024-03-01 11:48:59 +0000

    4th FE deploy

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -6,7 +6,7 @@ const ImageModal = ({ imageUrl, keywords, category, onClose }) => {
     return (
       <div className="image-modal">
         <div className="modal-content">
-          <img className="modal-image" src={imageUrl} alt="modal-image" />
+          {/* <img className="modal-image" src={imageUrl} alt="modal-image" /> */}
           <div className='image-details'>
             <p>Keywords: {keywords}</p>
             <p>Category: {category}</p>
